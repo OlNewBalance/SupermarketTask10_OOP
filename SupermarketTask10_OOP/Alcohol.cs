c18644ea56a9cf8fc89bf4106d01e0b85d103999
Oleg <finkton23@gmail.com>
2025-08-14 20:28:14 +0500

    Update Alcohol.cs

--- SupermarketTask10_OOP/Alcohol.cs
+++ SupermarketTask10_OOP/Alcohol.cs
@@ -1,6 +1,12 @@
-﻿namespace Task10;
+namespace Task10;
 
-public class Alcohol
+public class Alcohol : Goods
 {
-    
+    public Alcohol() : base(
+        name: "Алкоголь",
+        price: 3,
+        quantity: 5
+    )
+    {
+    }
 }
