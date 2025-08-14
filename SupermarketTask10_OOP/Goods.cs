8916d6289aa02516a15467b315e303f4721894b4
Oleg <finkton23@gmail.com>
2025-08-14 20:30:45 +0500

    Update Goods.cs

--- SupermarketTask10_OOP/Goods.cs
+++ SupermarketTask10_OOP/Goods.cs
@@ -1,6 +1,15 @@
-﻿namespace Task10;
+namespace Task10;
 
 public class Goods
 {
-        
+    public string Name;
+    public double Price { get; }
+    public int Quantity;
+
+    public Goods(string name, int quantity, double price)
+    {
+        Name = name;
+        Quantity = quantity;
+        Price = price;
+    }
 }
